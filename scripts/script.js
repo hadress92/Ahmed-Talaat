a29c7d9075006d8c4d418471b7d2f99af2a820c5
Ahmed Talaat <ahmed.talaat@creiden.com>
2022-02-10 01:15:44 +0200

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+// const rotatingIcon = document.querySelector(".rotating-icon")
+// const rotatedIcon = document.querySelector(".rotated-icon")
+
+
+// rotatingIcon.addEventListener('click',function(event) {
+// console.log(event)
+//     rotatingIcon.style.display="hidden"
+// })
